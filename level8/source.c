e6c8cad23428d4fb005e9cf4a1ceacb301a6cc82
lsaidi <lahcen.saidi@cleed.com>
2022-02-26 22:37:45 +0100

    init source level8

--- level8/source.c
+++ level8/source.c
@@ -4,10 +4,21 @@ char *auth = NULL;
 char *service = NULL;
 char buff[128];
 
-int main(int ac ,char *av[]){
+int main(){
 	while(fgets(buff, 128, stdin) != NULL){
 		printf("%p, %p \n", auth , service);
-		if (strcmp())
+		if (strncmp(buff, "auth ", 5) == 0){
+			auth = malloc(sizeof(char *));
+			//if (strlen(buff + 5))
+			strcpy(auth, "12345678901234567890")
+		}
+		else if (strlen(buff + 5))
+		{
+			strncmp(buff, "service", 7) == 0
+		}
+		else if(strncmp(buff, "reset", 5) == 0){
+			free(auth)
+		}
+
 	}
-	return 0;
 }
